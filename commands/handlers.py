d5230c99b23cd0eb3534832f6a469b1be4e543bd
a.druzhkin <amdruzhkin@gmail.com>
2024-06-22 15:07:52 +0300

    Hello message

--- commands/handlers.py
+++ commands/handlers.py
@@ -19,7 +19,9 @@ async def start_handler(message: Message):
     #     await create_user(user)
     #
     # keyboard = get_start_menu_kb(user.language)
-    await message.bot.send_message(chat_id=os.getenv('MANAGEMENT_CHAT_ID'), text='Сообщение в менеджмент')
-    await message.bot.send_message(chat_id=os.getenv('REVIEWS_CHAT_ID'), text='Сообщение в отзывы')
-    await message.bot.send_message(chat_id=os.getenv('NEWS_CHANNEL_ID'), text='Сообщение в новости')
+
     await message.answer(text='Добро пожаловать в CREALICE! Мы предоставляем услуги 3D-Печати на территории черноморского побережья.')
+    # await message.bot.send_message(chat_id=os.getenv('NEWS_CHANNEL_ID'), text='Сообщение в новости')
+    # await message.bot.send_message(chat_id=os.getenv('MANAGEMENT_CHAT_ID'), text='Сообщение в менеджмент')
+    # await message.bot.send_message(chat_id=os.getenv('REVIEWS_CHAT_ID'), text='Сообщение в отзывы')
+
